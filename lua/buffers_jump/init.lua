8bb2621b48ffd833eb28459b0705671290cbf9b4
Kamiloid <andres.camilo.ld@gmail.com>
2025-11-13 00:11:40 -0500

    feat:Implementing trim function for string vars

--- lua/buffers_jump/init.lua
+++ lua/buffers_jump/init.lua
@@ -1,15 +1,24 @@
 local M ={}
+local API = {
+	str_trim = function(s)
+		return s:match("^%s*(.-)%s*$")
+	end
+}
 
 function Get_Buffers()
 	local buffers = vim.api.nvim_list_bufs()
 	local count = 0
 	local names = {}
+	local cb_name = vim.api.nvim_buf_get_name(0)
 	for k, v in pairs(buffers) do
 		local name = vim.api.nvim_buf_get_name(v)
-		local trim = _G.str_trim(name)
-		if trim ~= "" then
+		local trim = API.str_trim(name)
+		if trim ~= "" and trim ~= API.str_trim(cb_name) then
 			table.insert(names, trim)
 		end
+	end
+	if API.str_trim(cb_name) ~= "" then
+		table.insert(names, 2, cb_name)
 	end
 	return names
 end
@@ -18,13 +27,17 @@ function Select()
 	local buffers = Get_Buffers()
 
 	if #buffers == 0 then
-		_G.console.log("I do not have non-empty buffers")
+		vim.notify("I do not have non-empty buffers", vim.log.levels.INFO)
 		return
 	end
 
-	vim.ui.select(buffers, { prompt = "Elige un buffer:" }, function(choice)
+	vim.ui.select(buffers, { prompt = "Active Buffers ("..#buffers..")" }, function(choice)
 		if choice then
-			vim.cmd("edit " .. choice)
+			local command = "edit " .. choice
+			local open, _ = pcall(vim.cmd, command)
+			if open == true then
+				vim.cmd(command)
+			end
 		end
 	end)
 end
